9e6eda0fd6672ac549d2372d58a459d012c10b14
Chaitra <chaitra.srinivas@gmail.com>
2021-12-03 21:54:35 +1100

    switched from promises to async await

--- controllers/api/events-routes.js
+++ controllers/api/events-routes.js
@@ -3,39 +3,6 @@ const { Events, Properties } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
 
-/* router.post("/", withAuth, (req, res) => {
-  Properties.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-  })
-    .then((propertyData) => {
-      if (propertyData.length == 0) {
-        res.status(404).json({ message: "No property found for this user" });
-        return;
-      } else {
-        const property = propertyData.map((prop) => prop.get({ plain: true }));
-        Events.create({
-          property_id: property[0].id,
-          event_id: req.body.event_id,
-          event_start_dt: req.body.start_date,
-          event_end_dt: req.body.end_date,
-          event_start_time: req.body.start_time,
-          event_end_time: req.body.end_time,
-        })
-          .then((eventData) => res.json(eventData))
-          .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-          });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-}); */
-
 router.post("/", withAuth, async (req, res) => {
   try {
     const propertyData = await Properties.findAll({
