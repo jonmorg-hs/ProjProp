f3eb11383e4687faec5ee36f1e97be9c03f7a138
Chaitra <chaitra.srinivas@gmail.com>
2021-12-03 21:28:00 +1100

    refactored

--- controllers/api/events-routes.js
+++ controllers/api/events-routes.js
@@ -3,7 +3,7 @@ const { Events, Properties } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, (req, res) => {
+/* router.post("/", withAuth, (req, res) => {
   Properties.findAll({
     where: {
       user_id: req.session.user_id,
@@ -34,6 +34,36 @@ router.post("/", withAuth, (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+}); */
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const propertyData = await Properties.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (propertyData.length == 0) {
+      res.status(404).json({ message: "No property found for this user" });
+      return;
+    } else {
+      const property = propertyData.map((prop) => prop.get({ plain: true }));
+      const eventData = await Events.create({
+        property_id: property[0].id,
+        event_id: req.body.event_id,
+        event_start_dt: req.body.start_date,
+        event_end_dt: req.body.end_date,
+        event_start_time: req.body.start_time,
+        event_end_time: req.body.end_time,
+      });
+
+      res.json(eventData);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", withAuth, (req, res) => {
