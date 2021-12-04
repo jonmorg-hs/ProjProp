737ed8b96f012053dd599b3fe7d999fca7dd9d04
Jon Morgan <jonmorg@iinet.net.au>
2021-12-04 17:29:56 +1100

    fixed upload image and shorter sound for halloween search

--- public/js/register-event.js
+++ public/js/register-event.js
@@ -39,11 +39,10 @@ document
   .querySelector('input[type="file"]')
   .addEventListener("change", function () {
     if (this.files && this.files[0]) {
-      var img = document.querySelector("myImg");
+      var img = document.querySelector("#myImg");
       img.onload = () => {
         URL.revokeObjectURL(img.src); // no longer needed, free memory
       };
-
       img.src = URL.createObjectURL(this.files[0]); // set src to blob url
     }
   });
